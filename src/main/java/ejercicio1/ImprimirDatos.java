3b5264b5b7005e384b0284b66aa11dc76363fbc9
felipe <pipega2000@hotmail.com>
2022-12-13 01:52:25 -0500

    Tarea terminada

---
+++ src/main/java/ejercicio1/ImprimirDatos.java
@@ -0,0 +1,16 @@
+package ejercicio1;
+
+public class ImprimirDatos {
+
+    public static void main(String[] args) {
+
+
+        String cadena = "hola";
+        int entero = 1;
+        boolean falso = false;
+        double doble = 2.3;
+        Long extenso = 100L;
+
+        System.out.println(cadena + " "+ entero + " "+ falso + " "+ doble + " "+ extenso + " ");
+    }
+}
